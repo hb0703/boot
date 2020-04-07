476b71571cf1a6e15bdf7a4cde528b1e602fcb1d
黄波 <1256897220@qq.com>
2020-04-07 20:31:34 +0800

    测试

--- src/main/java/com/hb/demo/DemoApplication.java
+++ src/main/java/com/hb/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableScheduling //开启定时job
 public class DemoApplication {
 
 	public static void main(String[] args) {
